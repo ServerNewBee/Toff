8d027393576dea4835ff1f9599c0eac96eb0d7da
tianyu.wang <tianyu.wang@yitu-inc.com>
2017-05-01 16:28:31 +0800

    change file

--- src/error_code.h
+++ src/error_code.h
@@ -5,4 +5,6 @@
 #ifndef TOFF_ERROR_CODE_H
 #define TOFF_ERROR_CODE_H
 
+#define TOFF_POINTER_IS_NULL -1
+
 #endif //TOFF_ERROR_CODE_H
---
+++ src/hello_world.cpp
@@ -0,0 +1,11 @@
+//
+// Created by tywang on 17-5-1.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    return 0;
+}
